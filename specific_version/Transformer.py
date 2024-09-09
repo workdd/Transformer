3be26e7eddaa70568175db25bea751c322ba42ec
최재강 <workd.official@gmail.com>
2024-09-10 00:42:28 +0900

    add specific transfoermer

--- specific_version/Transformer.py
+++ specific_version/Transformer.py
@@ -118,3 +118,207 @@ class PositionwiseFeedForward(nn.Module):
         x = self.dropout(x)
         x = self.linear2(x)
         return x
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
+        super(EncoderLayer, self).__init__()
+
+        self.attention = MultiHeadAttention(d_model, n_head)
+
+        self.norm1 = LayerNorm(d_model=d_model)
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.ffn = PositionwiseFeedForward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+
+        self.norm2 = LayerNorm(d_model=d_model)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+    def forward(self, x, src_mask):
+        _x = x  # residual connection을 위한
+
+        x = self.attention(q=x, k=x, v=x, mask=src_mask)
+
+        x = self.norm1(x + _x)
+        x = self.dropout1(x)
+
+        _x = x
+        x = self.ffn(x)
+
+        x = self.norm2(x + _x)
+        x = self.dropout2(x)
+
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(self, enc_voc_size, max_len, d_model, ffn_hidden, n_head, n_layers, drop_prob, device):
+        super().__init__()
+
+        self.embed = nn.Embedding(num_embeddings=len(kor_text.vocab), embedding_dim=d_model, padding_idx=1)
+
+        self.pe = PositionalEncoding(max_len=max_len, d_model=d_model, device=device)
+
+        self.layers = nn.ModuleList([EncoderLayer(d_model=d_model, ffn_hidden=ffn_hidden, n_head=n_head, drop_prob=drop_prob) for _ in range(n_layers)])
+
+    def forward(self, x, src_mask):
+        x = self.embed(x)
+
+        x_pe = self.pe(x)
+
+        x = x + x_pe
+
+        for layer in self.layers:
+            x = layer(x, mask=src_mask)
+
+        return x
+
+
+class DecoderLayer(nn.ModuleFinder):
+    def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
+        super(DecoderLayer, self).__init__()
+
+        self.self_attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+
+        self.norm1 = LayerNorm(d_model=d_model)
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.enc_dec_attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+
+        self.norm2 = LayerNorm(d_model=d_model)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+        self.ffn = PositionwiseFeedForward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+
+        self.norm3 = LayerNorm(d_model=d_model)
+        self.dropout3 = nn.Dropout(p=drop_prob)
+
+    def forward(self, dec, enc, trg_mask, src_mask):
+        _x = dec
+
+        x = self.self_attention(q=dec, k=dec, v=dec, mask=trg_mask)
+
+        x = self.norm1(x + _x)
+        x = self.dropout1(x)
+
+        if enc is not None:
+            _x = x
+
+            # encoder - decoder attention 계산
+            # Query(q): decoder attention output
+            # Key(k): Encoder output
+            # Value(v): Encoder output
+
+            x = self.enc_dec_attention(q=x, k=enc, v=enc, mask=src_mask)
+
+            x = self.norm2(x + _x)
+            x = self.dropout2(x)
+
+        _x = x
+
+        x = self.ffn(x)
+
+        x = self.norm3(x + _x)
+        x = self.dropout3(x)
+
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, dec_voc_size, max_len, d_model, ffn_hidden, n_head, n_layers, drop_prob, device):
+        super().__init__()
+
+        self.embed = nn.Embedding(num_embeddings=len(eng_text.vocab), embedding_dim=d_model, padding_idx=1)
+
+        self.pe = PositionalEncoding(max_len=50, d_model=d_model, device="cuda")
+
+        self.layers = nn.ModuleList([DecoderLayer(d_model=d_model, ffn_hidden=ffn_hidden, n_head=n_head, drop_prob=drop_prob) for _ in range(n_layers)])
+
+        self.linear = nn.Linear(d_model, dec_voc_size)
+
+    def forward(self, trg, src, trg_mask, src_mask):
+
+        trg = self.embed(trg)
+        trg_pe = self.pe(trg)
+        trg = trg + trg_pe  # Embedding + Positional Encoding
+
+        for layer in self.layers:
+            trg = layer(trg, src, trg_mask, src_mask)
+
+        output = self.linear(trg)
+
+        return output
+
+
+class Transforemnr(nn.Module):
+    def __init__(self, src_pad_idx, trg_pad_idx, trg_sos_idx, enc_voc_size, dec_voc_size, d_model, n_head, max_len, ffn_hidden, n_layers, drop_prob, device):
+        super().__init__()
+
+        # Get <PAD> idx
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.trg_sos_idx = trg_sos_idx
+
+        # Encoder
+        self.encoder = Encoder(
+            enc_voc_size=enc_voc_size,
+            max_len=max_len,
+            d_model=d_model,
+            ffn_hidden=ffn_hidden,
+            n_head=n_head,
+            n_layers=n_layers,
+            drop_prob=drop_prob,
+            device=device,
+        )
+
+        # Decoder
+        self.decoder = Decoder(
+            dec_voc_size=dec_voc_size,
+            max_len=max_len,
+            d_model=d_model,
+            ffn_hidden=ffn_hidden,
+            n_head=n_head,
+            n_layers=n_layers,
+            drop_prob=drop_prob,
+            device=device,
+        )
+
+        self.device = device
+
+    def make_pad_mask(self, q, k):
+        # Padding 부분은 masking을 하여 계산에서 제외되도록 함
+
+        len_q, len_k = q.size(1), k.size(1)
+
+        # batch_size x 1  x 1 x len_k
+        k = k.ne(self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+
+        # batch_size x 1 x 1 x len_k
+        k = k.repeat(1, 1, len_q, 1)
+
+        # batch_size x 1 x len_q x 1
+        q = q.ne(self.src_pad_idx).unsqueeze(2).unsqueeze(3)
+
+        # batch_size x 1 x len_q x len_k
+        q = q.repeat(1, 1, 1, len_k)
+
+        mask = k & q
+        return mask
+
+    def make_no_peak_mask(self, q, k):
+        len_q, len_k = q.size(1), k.size(1)
+
+        mask = torch.trill(torch.ones(len_q, len_k).type(torch.BoolTensor).to(self.device))
+
+        return mask
+
+    def forward(self, src, trg):
+        src_mask = self.make_pad_mask(src, src)
+        src_trg_mask = self.make_pad_mask(trg, src)
+        trg_mask = self.maks_pad_mask(trg, trg) * self.make_no_peak_mask(trg, trg)
+
+        enc_src = self.encoder(src, src_mask)
+
+        output = self.decoder(trg, enc_src, trg_mask, src_trg_mask)
+
+        return output
